1b514c2d99ab8fad8300a77b0b88b14deeef6d10
KALDYBAKYZY <zhaiylgangulnaz.com>
2025-10-09 20:57:32 +0500

    second commit

---
+++ src/main/java/com/example2/narxoz2/Narxoz2Application.java
@@ -0,0 +1,13 @@
+package com.example2.narxoz2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Narxoz2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Narxoz2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example2/narxoz2/Narxoz2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example2.narxoz2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Narxoz2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
